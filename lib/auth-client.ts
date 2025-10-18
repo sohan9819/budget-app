737c8614f50fd5b21c9b7d50a02be854ba688ce3
sohan <sohanshetty2001@gmail.com>
2025-10-18 19:48:21 +0530

    DEPLOYMENT_FIX:added env base url to auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -9,7 +9,7 @@ export const {
   resetPassword,
 } = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BETTER_AUTH_URL,
 });
 
 export const googleSignIn = async () => {
